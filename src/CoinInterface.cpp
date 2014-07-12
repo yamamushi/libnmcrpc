c3d9d6bff2626efab21eba59722b36274eb479f6
Daniel Kraft <d@domob.eu>
2014-07-12 11:28:25 +0200

    Copy currency-related interface stuff in new CoinInterface class.
    Create a new class "CoinInterface" that holds all currency-related
    things (plus Address and WalletUnlocker) of the "NamecoinInterface".  In
    a next step, NamecoinInterface will be made a subclass of it, and only
    add the name related stuff.

---
+++ src/CoinInterface.cpp
@@ -0,0 +1,290 @@
+/*  Namecoin RPC library.
+ *  Copyright (C) 2013-2014  Daniel Kraft <d@domob.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  See the distributed file COPYING for additional permissions in addition
+ *  to those of the GNU Affero General Public License.
+ */
+
+/* Source code for CoinInterface.hpp.  */
+
+#include "CoinInterface.hpp"
+
+#include <ctime>
+#include <sstream>
+
+namespace nmcrpc
+{
+
+/* ************************************************************************** */
+/* High-level interface to the core wallet.  */
+
+const unsigned CoinInterface::UNLOCK_SECONDS = 3600;
+
+/**
+ * Run a simple test command and return whether the connection seems to
+ * be up and running.  It also produces a message that can be shown, which
+ * either includes the running version or an error string.
+ * @param msg Set this to the message string.
+ * @return True iff the connection seems to be fine.
+ */
+bool
+CoinInterface::testConnection (std::string& msg)
+{
+  try
+    {
+      const JsonRpc::JsonData res = rpc.executeRpc ("getinfo");
+      int version = res["version"].asInt ();
+
+      unsigned v3 = version % 100;
+      version /= 100;
+      unsigned v2 = version % 100;
+      version /= 100;
+
+      std::ostringstream msgOut;
+      msgOut << "Success!  Daemon version "
+             << "0." << version << "." << v2;
+      if (v3 > 0)
+        msgOut << "." << v3;
+      msgOut << " running.";
+      msg = msgOut.str ();
+
+      return true;
+    }
+  catch (const JsonRpc::HttpError& exc)
+    {
+      std::ostringstream res;
+      res << "HTTP-Error (" << exc.getResponseCode () << "): "
+          << exc.what ();
+      msg = res.str ();
+    }
+  catch (const JsonRpc::Exception& exc)
+    {
+      msg = exc.what ();
+    }
+
+  return false;
+}
+
+/**
+ * Query for an address by string.  This immediately checks whether the
+ * address is valid and owned by the user, so that this information can be
+ * encapsulated into the returned object.
+ * @param addr The address to check.
+ * @return The created address object.
+ */
+CoinInterface::Address
+CoinInterface::queryAddress (const std::string& addr)
+{
+  return Address (rpc, addr);
+}
+
+/**
+ * Create a new address (as per "getnewaddress") and return it.
+ * @return Newly created address.
+ */
+CoinInterface::Address
+CoinInterface::createAddress ()
+{
+  const JsonRpc::JsonData addr = rpc.executeRpc ("getnewaddress");
+  return Address (rpc, addr.asString ());
+}
+
+/**
+ * Query for the number of confirmations a transaction has.
+ * @param txid The transaction id to check for.
+ * @return Number of confirmations the transaction has.
+ * @throws JsonRpc::RpcError if the tx is not found.
+ */
+unsigned
+CoinInterface::getNumberOfConfirmations (const std::string& txid)
+{
+  const JsonRpc::JsonData res = rpc.executeRpc ("gettransaction", txid);
+  assert (res.isObject ());
+
+  return res["confirmations"].asInt ();
+}
+
+/**
+ * Check whether the wallet needs to be unlocked or not.  This routine is
+ * used to decide whether we need to ask for a passphrase or not before
+ * using WalletUnlocker.
+ * @return True iff we need a passphrase.
+ */
+bool
+CoinInterface::needWalletPassphrase ()
+{
+  const JsonRpc::JsonData res = rpc.executeRpc ("getinfo");
+
+  if (res["unlocked_until"].isNull ())
+    return false;
+
+  const int until = res["unlocked_until"].asInt ();
+  return (until < std::time (nullptr) + UNLOCK_SECONDS);
+}
+
+/* ************************************************************************** */
+/* Address object.  */
+
+/**
+ * Construct the address.  This is meant to be used only
+ * from inside CoinInterface.  Outside users should use
+ * CoinInterface::queryAddress or other methods to obtain
+ * address objects.
+ * @param r The namecoin interface to use.
+ * @param a The address as string.
+ */
+CoinInterface::Address::Address (JsonRpc& r, const std::string& a)
+  : rpc(&r), addr(a)
+{
+  const JsonRpc::JsonData res = rpc->executeRpc ("validateaddress", addr);
+  valid = res["isvalid"].asBool ();
+  mine = false;
+  if (valid)
+    mine = res["ismine"].asBool ();
+}
+
+/**
+ * Check a message signature against this address.  If this address
+ * is invalid, false is returned.
+ * @param msg The message that should be signed.
+ * @param sig The message's signature.
+ * @return True iff the signature matches the message.
+ */
+bool
+CoinInterface::Address::verifySignature (const std::string& msg,
+                                         const std::string& sig) const
+{
+  if (!valid)
+    return false;
+
+  try
+    {
+      JsonRpc::JsonData res = rpc->executeRpc ("verifymessage", addr, sig, msg);
+      return (res.isBool () && res.asBool ());
+    }
+  catch (const JsonRpc::RpcError& exc)
+    {
+      // Malformed base64?
+      if (exc.getErrorCode () == -5)
+        return false;
+      throw exc;
+    }
+}
+
+/**
+ * Sign a message with this address.  This may throw if the address
+ * is invalid, the wallet locked or the private key is not owned.
+ * @param msg The message that should be signed.
+ * @return The message's signature.
+ * @throws NoPrivateKey if this address is not owned.
+ * @throws std::runtime_error if the address is invalid or the wallet locked.
+ */
+std::string
+CoinInterface::Address::signMessage (const std::string& msg) const
+{
+  if (!valid)
+    throw std::runtime_error ("Can't sign with invalid address.");
+
+  try
+    {
+      const JsonRpc::JsonData res = rpc->executeRpc ("signmessage", addr, msg);
+      return res.asString ();
+    }
+  catch (const JsonRpc::RpcError& exc)
+    {
+      switch (exc.getErrorCode ())
+        {
+        case -13:
+          throw std::runtime_error ("Need to unlock the wallet first.");
+
+        case -3:
+          {
+            std::ostringstream msg;
+            msg << "You don't have the private key of " << addr << " in order"
+                << " to sign messages with that address.";
+            throw NoPrivateKey (msg.str ());
+          }
+
+        default:
+          throw exc;
+        }
+    }
+}
+
+/* ************************************************************************** */
+/* Wallet unlocker.  */
+
+/**
+ * Construct it, not yet unlocking.
+ * @param n The CoinInterface to use.
+ */
+CoinInterface::WalletUnlocker::WalletUnlocker (CoinInterface& n)
+  : rpc(n.rpc), nc(n), unlocked(false)
+{
+  // Nothing else to do.
+}
+
+/**
+ * Lock the wallet on destruct.
+ */
+CoinInterface::WalletUnlocker::~WalletUnlocker ()
+{
+  if (unlocked)
+    rpc.executeRpc ("walletlock");
+}
+
+/**
+ * Perform the unlock (if necessary).  The passphrase must be correct if the
+ * wallet is actually locked, and can be anything else.
+ * @param passphrase Passphrase to use for unlocking.
+ * @throws UnlockFailure if the passphrase is wrong.
+ */
+void
+CoinInterface::WalletUnlocker::unlock (const std::string& passphrase)
+{
+  if (unlocked)
+    throw std::runtime_error ("Wallet is already unlocked!");
+  
+  const bool needPwd = nc.needWalletPassphrase ();
+  if (needPwd)
+    {
+      /* Empty password is a special case, because it is not handled correctly
+         by the walletpassphrase RPC call.  Thus catch this case right now
+         and fail as if the passphrase would have been wrong.  */
+      if (passphrase.empty ())
+        throw UnlockFailure ("Wallet passphrase cannot be empty.");
+
+      /* Ensure the wallet is indeed locked before we send the passphrase.
+         It could be the case that it is unlocked although for too short
+         a time, then lock it now.  */
+      rpc.executeRpc ("walletlock");
+
+      try
+        {
+          rpc.disableLoggingOneShot ();
+          rpc.executeRpc ("walletpassphrase", passphrase, UNLOCK_SECONDS);
+          unlocked = true;
+        }
+      catch (const JsonRpc::RpcError& exc)
+        {
+          if (exc.getErrorCode () == -14)
+            throw UnlockFailure ("Wrong wallet passphrase.");
+          throw exc;
+        }
+    }
+}
+
+} // namespace nmcrpc
---
+++ src/CoinInterface.hpp
@@ -0,0 +1,383 @@
+/*  Namecoin RPC library.
+ *  Copyright (C) 2013-2014  Daniel Kraft <d@domob.eu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  See the distributed file COPYING for additional permissions in addition
+ *  to those of the GNU Affero General Public License.
+ */
+
+#ifndef NMCRPC_COININTERFACE_HPP
+#define NMCRPC_COININTERFACE_HPP
+
+#include "JsonRpc.hpp"
+
+#include <cassert>
+#include <stdexcept>
+#include <string>
+
+namespace nmcrpc
+{
+
+/* ************************************************************************** */
+/* High-level interface to the core wallet.  */
+
+/**
+ * Allow high-level interfacing to a coin daemon via an underlying RPC
+ * connection.  This is the part that is independent of names, just balances
+ * and addresses.  It can be used for Bitcoin or other coins as well as
+ * Namecoin.
+ */
+class CoinInterface
+{
+
+public:
+
+  /* Exceptions.  */
+  class NoPrivateKey;
+  class UnlockFailure;
+
+  /* Other child classes.  */
+  class Address;
+  class WalletUnlocker;
+
+private:
+
+  /** Underlying RPC connection.  */
+  JsonRpc& rpc;
+
+  /**
+   * The number of seconds we want to temporarily unlock the wallet in case
+   * we need access to a private key.
+   */
+  static const unsigned UNLOCK_SECONDS;
+
+  // Disable copying and default constructor.
+#ifndef CXX_11
+  CoinInterface ();
+  CoinInterface (const CoinInterface&);
+  CoinInterface& operator= (const CoinInterface&);
+#endif /* !CXX_11  */
+
+public:
+
+  /**
+   * Construct it with the given RPC connection.
+   * @param r The RPC connection.
+   */
+  explicit inline CoinInterface (JsonRpc& r)
+    : rpc(r)
+  {
+    // Nothing more to be done.
+  }
+
+  // We want no default constructor or copying.
+#ifdef CXX_11
+  CoinInterface () = delete;
+  CoinInterface (const CoinInterface&) = delete;
+  CoinInterface& operator= (const CoinInterface&) = delete;
+#endif /* CXX_11?  */
+
+  /**
+   * Run a simple test command and return whether the connection seems to
+   * be up and running.  It also produces a message that can be shown, which
+   * either includes the running version or an error string.
+   * @param msg Set this to the message string.
+   * @return True iff the connection seems to be fine.
+   */
+  bool testConnection (std::string& msg);
+
+  /**
+   * Run test command and discard message string.
+   * @return True iff the connection seems to be fine.
+   */
+  inline bool
+  testConnection ()
+  {
+    std::string msg;
+    return testConnection (msg);
+  }
+
+  /**
+   * Query for an address by string.  This immediately checks whether the
+   * address is valid and owned by the user, so that this information can be
+   * encapsulated into the returned object.
+   * @param addr The address to check.
+   * @return The created address object.
+   */
+  Address queryAddress (const std::string& addr);
+
+  /**
+   * Create a new address (as per "getnewaddress") and return it.
+   * @return Newly created address.
+   */
+  Address createAddress ();
+
+  /**
+   * Query for the number of confirmations a transaction has.
+   * @param txid The transaction id to check for.
+   * @return Number of confirmations the transaction has.
+   * @throws JsonRpc::RpcError if the tx is not found.
+   */
+  unsigned getNumberOfConfirmations (const std::string& txid);
+
+  /**
+   * Check whether the wallet needs to be unlocked or not.  This routine is
+   * used to decide whether we need to ask for a passphrase or not before
+   * using WalletUnlocker.
+   * @return True iff we need a passphrase.
+   */
+  bool needWalletPassphrase ();
+
+};
+
+/* ************************************************************************** */
+/* Address object.  */
+
+/**
+ * Encapsulate a Namecoin address.
+ */
+class CoinInterface::Address
+{
+
+private:
+
+  friend class CoinInterface;
+
+  /** The namecoin interface to be used for RPC calls (message signing).  */
+  JsonRpc* rpc;
+
+  /** The address as string.  */
+  std::string addr;
+
+  /** Whether the address is valid.  */
+  bool valid;
+  /** Whether the address is owned by the user.  */
+  bool mine;
+
+  /**
+   * Construct the address.  This is meant to be used only
+   * from inside CoinInterface.  Outside users should use
+   * CoinInterface::queryAddress or other methods to obtain
+   * address objects.
+   * @param r The namecoin interface to use.
+   * @param a The address as string.
+   */
+  Address (JsonRpc& r, const std::string& a);
+
+public:
+
+  /**
+   * Default constructor, initialises to "".  This is here for convenience
+   * so that variables of type Address can be declared.
+   */
+  inline Address ()
+    : rpc(nullptr), addr(""), valid(false), mine(false)
+  {
+    // Nothing more to do.
+  }
+
+  // Copying is ok.
+#ifdef CXX_11
+  Address (const Address&) = default;
+  Address& operator= (const Address&) = default;
+#endif /* CXX_11?  */
+
+  /**
+   * Get the address as string.
+   * @return The address as string.
+   */
+  inline const std::string&
+  getAddress () const
+  {
+    return addr;
+  }
+
+  /**
+   * Get whether or not the address is valid.
+   * @return True iff this is a valid address.
+   */
+  inline bool
+  isValid () const
+  {
+    return valid;
+  }
+
+  /**
+   * Get whether or not the address is owned by the user.
+   * @return True iff the user has the address' private key.
+   */
+  inline bool
+  isMine () const
+  {
+    return mine;
+  }
+
+  /**
+   * Check a message signature against this address.  If this address
+   * is invalid, false is returned.
+   * @param msg The message that should be signed.
+   * @param sig The message's signature.
+   * @return True iff the signature matches the message.
+   */
+  bool verifySignature (const std::string& msg, const std::string& sig) const;
+
+  /**
+   * Sign a message with this address.  This may throw if the address
+   * is invalid, the wallet locked or the private key is not owned.
+   * @param msg The message that should be signed.
+   * @return The message's signature.
+   * @throws NoPrivateKey if this address is not owned.
+   * @throws std::runtime_error if the address is invalid or the wallet locked.
+   */
+  std::string signMessage (const std::string& msg) const;
+
+};
+
+/* ************************************************************************** */
+/* Wallet unlocker.  */
+
+/**
+ * Unlock the wallet temporarily (during the scope of the object).  RAII
+ * ensures that the wallet is locked (if it was indeed unlocked) again
+ * at the latest when we go out of scope.
+ */
+class CoinInterface::WalletUnlocker
+{
+
+private:
+
+  friend class CoinInterface;
+
+  /** The RPC object to use.  */
+  JsonRpc& rpc;
+  /** High-level interface to use.  */
+  CoinInterface& nc;
+
+  /** Whether we actually unlocked the wallet.  */
+  bool unlocked;
+
+  // Disable copying and default constructor.
+#ifndef CXX_11
+  WalletUnlocker ();
+  WalletUnlocker (const WalletUnlocker&);
+  WalletUnlocker& operator= (const WalletUnlocker&);
+#endif /* !CXX_11  */
+
+public:
+
+  /**
+   * Construct it, not yet unlocking.
+   * @param n The CoinInterface to use.
+   */
+  explicit WalletUnlocker (CoinInterface& n);
+
+  // No default constructor or copying.
+#ifdef CXX_11
+  WalletUnlocker () = delete;
+  WalletUnlocker (const WalletUnlocker&) = delete;
+  WalletUnlocker& operator= (const WalletUnlocker&) = delete;
+#endif /* CXX_11?  */
+
+  /**
+   * Lock the wallet on destruct.
+   */
+  ~WalletUnlocker ();
+
+  /**
+   * Perform the unlock (if necessary).  The passphrase must be correct if the
+   * wallet is actually locked, and can be anything else.
+   * @param passphrase Passphrase to use for unlocking.
+   * @throws UnlockFailure if the passphrase is wrong.
+   */
+  void unlock (const std::string& passphrase);
+
+};
+
+/* ************************************************************************** */
+/* Exception classes.  */
+
+/**
+ * Thrown when an action is to be performed for which the private key
+ * is missing from the wallet.
+ */
+class CoinInterface::NoPrivateKey : public std::runtime_error
+{
+
+private:
+
+  // Disable default constructor.
+#ifndef CXX_11
+  NoPrivateKey ();
+#endif /* !CXX_11  */
+
+public:
+
+  /**
+   * Construct it given the error message.
+   * @param msg The error message.
+   */
+  explicit inline NoPrivateKey (const std::string& msg)
+    : std::runtime_error(msg)
+  {
+    // Nothing else to do.
+  }
+
+  /* No default constructor, but copying ok.  */
+#ifdef CXX_11
+  NoPrivateKey () = delete;
+  NoPrivateKey (const NoPrivateKey&) = default;
+  NoPrivateKey& operator= (const NoPrivateKey&) = default;
+#endif /* CXX_11?  */
+
+};
+
+/**
+ * Thrown in case of failure to unlock the wallet (because of a wrong
+ * passphrase, presumably).
+ */
+class CoinInterface::UnlockFailure : public std::runtime_error
+{
+
+private:
+
+  // Disable default constructor.
+#ifndef CXX_11
+  UnlockFailure ();
+#endif /* !CXX_11  */
+
+public:
+
+  /**
+   * Construct it given the error message.
+   * @param msg The error message.
+   */
+  explicit inline UnlockFailure (const std::string& msg)
+    : std::runtime_error(msg)
+  {
+    // Nothing else to do.
+  }
+
+  /* No default constructor, but copying ok.  */
+#ifdef CXX_11
+  UnlockFailure () = delete;
+  UnlockFailure (const UnlockFailure&) = default;
+  UnlockFailure& operator= (const UnlockFailure&) = default;
+#endif /* CXX_11?  */
+
+};
+
+} // namespace nmcrpc
+
+#endif /* Header guard.  */
